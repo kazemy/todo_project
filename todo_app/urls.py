5eb44bb097bf69f60bd7a27097207ac1143169fa
Kazemy <a.kazemy@gmail.com>
2020-08-27 16:28:41 -0400

    completed - added delete and delete all functionality

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add', views.addTodo, name='add'),
+    path('complete/<int:todo_id>', views.completeTodo, name='complete'),
+    path('deletecomplete', views.deleteCompleted, name='deletecomplete'),
+    path('deleteall', views.deleteAll, name='deleteall'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
 from .forms import TodoForm
 
@@ -8,3 +8,25 @@ def index (request):
     form = TodoForm()
     context = {'todo_list': todo_list, 'form': form}
     return render(request, 'todo_app/index.html', context)
+
+def addTodo(request):
+    form = TodoForm(request.POST)
+  
+    if form.is_valid():
+        new_todo = Todo(text=request.POST['text'])
+        new_todo.save()
+    return redirect('index')
+
+def completeTodo(request, todo_id):
+    todo = Todo.objects.get(pk=todo_id)
+    todo.complete = True
+    todo.save()
+    return redirect('index')
+
+def deleteCompleted(request):
+    Todo.objects.filter(complete__exact=True).delete()
+    return redirect('index')
+
+def deleteAll(request):
+    Todo.objects.all().delete()
+    return redirect('index')
